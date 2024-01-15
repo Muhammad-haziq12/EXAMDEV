36168286c6ef2fa5b0a07853f4934107ca39fdbd
Muhammad-Haziq12 <muhammad.haziq@student.vinci.be>
2024-01-15 16:54:29 +0100

    h

--- src/services/validations.js
+++ src/services/validations.js
@@ -3,8 +3,7 @@ const isValid = (gamerTag) => {
     if (gamerTag.length < 8) {
       return "Invalid - gamertag length must be at least 8 characters";
     }
-  
-    
+
     const specialCharacters = ['&', '$', '!', 'è', '§', 'à', '_'];
     if (!gamerTag.split('').some(char => specialCharacters.includes(char))) {
       return "Invalid - gamertag must contain at least a special character";
