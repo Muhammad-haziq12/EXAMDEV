47ba6ba35066c52874e7206dfbb557053202b331
Muhammad-Haziq12 <muhammad.haziq@student.vinci.be>
2024-01-15 16:41:21 +0100

    add:funtion and tests

--- src/services/validations.js
+++ src/services/validations.js
@@ -1,4 +1,21 @@
-// TODO: Refactor validation function to follow the defined validations rules
-const isValid = () => Math.random() >= 0.5;
 
-exports.isValid = isValid;
+const isValid = (gamerTag) => {
+    if (gamerTag.length < 8) {
+      return "Invalid - gamertag length must be at least 8 characters";
+    }
+  
+    
+    const specialCharacters = ['&', '$', '!', 'è', '§', 'à', '_'];
+    if (!gamerTag.split('').some(char => specialCharacters.includes(char))) {
+      return "Invalid - gamertag must contain at least a special character";
+    }
+  
+    if (!/\d/.test(gamerTag)) {
+      return "Invalid - gamertag must contain at least a number";
+    }
+  
+    return "Valid - gamertag is valid";
+  }
+  
+  exports.isValid = isValid;
+  
